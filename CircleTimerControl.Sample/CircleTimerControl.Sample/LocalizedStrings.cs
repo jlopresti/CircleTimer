1c3f0ee9afa76fec70b9e8bbbe134d13637fde28
jlopresti <julien.lopresti@outlook.com>
2013-05-30 03:08:20 +0200

    init

---
+++ CircleTimerControl.Sample/CircleTimerControl.Sample/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using CircleTimerControl.Sample.Resources;
+
+namespace CircleTimerControl.Sample
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
